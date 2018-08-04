76a5c7fb1e890f41ccf60f28fcd0252750412cbd
msicakkanli <msicakkanli@protein.tech>
2018-08-04 23:08:24 +0300

    add authenticate

--- src/models/users.js
+++ src/models/users.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bycrpt = require('bcrypt')
+
 const userSchema = new mongoose.Schema({
     fullName: {
         type: String,
@@ -7,7 +9,7 @@ const userSchema = new mongoose.Schema({
     },
     emailAddress : {
         type: String,
-         unique: true,
+         unique: [true, 'The e-mail adress is already exist'],
          required: true,
     },
     password: {
@@ -15,6 +17,38 @@ const userSchema = new mongoose.Schema({
         required: true,
     }
 });
+//authenticate user data 
+userSchema.statics.authenticate = function (emailAddress, password, callback) {
+    User.findOne({ emailAddress : emailAddress})
+    .exec(function (error, user) {
+        if (error) {
+            return callback(error)
+        } else if (!user)  {
+            let err = new Error('User not found!')
+            err.status = 401;
+            return callback (err);
+        }
+        bycrpt.compare(password, user.password, function (error, result) {
+            if (result === true) {
+                return callback(null, user )
+            } else {
+                return callback();
+            }
+        })
+    })
+}
+
+//hash password 
+userSchema.pre('save', function (next) {
+    let user = this;
+    bycrpt.hash(user.password, 10 , function (err, hash) {
+        if(err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
+})
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -26,14 +26,27 @@ router.post('/users',function (req,res,next) {
           return next(err);
         }
 
+        User.authenticate(req.body.password, req.body.emailAddress, function (error, user) {
+          if (error || !user) {
+            let err = new Error ('Wrong email or password')
+            err.status = 401 
+            return next(err)
+          } else {
+            req.session.userId = user._id
+            return res.send('user authenticate')
+          }
+        })
+
         let userData = {
           fullName : req.body.fullName,
           emailAddress : req.body.emailAddress,
           password: req.body.password
         };
-        User.create(userData, function (error,user) {
-          if (error) {
-            return next(error)
+        User.create(userData, function (err,user) {
+          if (err) {
+            const err = new Error('The email adress is already exist')
+            err.status = 401
+            return next(err)
           }else {
             return res.json(user);
           }
